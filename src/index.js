34d65b78626057eade7c0d87224ac8a38a447e9c
Daudi Mwanzia <daudimwanzia@gmail.com>
2023-10-07 09:15:25 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevents the default form submission behavior
+
+    // Get the task description from the input field
+    const taskDescriptionInput = document.getElementById(
+      "new-task-description"
+    );
+    const taskDescription = taskDescriptionInput.value.trim();
+
+    // Check if the input is not empty
+    if (taskDescription !== "") {
+      // Create a new list item
+      const listItem = document.createElement("li");
+      listItem.textContent = taskDescription;
+
+      // Create Delete button
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function () {
+        // Implement the delete functionality here
+        taskList.removeChild(listItem);
+      });
+
+      // Append buttons to the list item
+      listItem.appendChild(deleteButton);
+
+      // Add the new task to the task list
+      taskList.appendChild(listItem);
+
+      // Clear the input field
+      taskDescriptionInput.value = "";
+    } else {
+      // Log a message if the input is empty
+      console.log("Please enter a task description.");
+    }
+  });
 });
